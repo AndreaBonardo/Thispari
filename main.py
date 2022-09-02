a7b19b3c67d2f205109939d6007bbaeeafdbbb8b
AndreaBonardo <98973869+AndreaBonardo@users.noreply.github.com>
2022-09-02 10:14:15 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,87 @@
+from flask  import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
+
+app = Flask(
+	__name__,
+	template_folder='templates',
+	static_folder='static'
+)
+
+app.secret_key = '8845.Ab.'
+  
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '8845.Ab.'
+app.config['MYSQL_DB'] = 'geeklogin'
+  
+mysql = MySQL(app)
+
+@app.route('/')
+def intro_page():
+	return render_template("intro.html")
+
+@app.route('/login')
+def login():
+	return render_template("login.html")
+
+@app.route('/login',methods = ['POST', 'GET'])
+def loginerrori():
+  LoginPassword = request.form["Password"]
+  LoginEmail = request.form["Email"]
+  if LoginPassword == "":
+    return render_template("errore2.html")
+  elif LoginEmail == "":
+    return render_template("errore2.html")
+  else:
+    return redirect(url_for('base_page'))
+
+@app.route('/register')
+def register():
+  return render_template("register.html")
+
+@app.route('/register',methods = ['POST', 'GET'])
+def errori():
+  Password = request.form["Password"]
+  Email = request.form["Email"]
+  Nome = request.form["Nome"]
+  Cognome = request.form["Cognome"]
+  RipetiPassword = request.form["RipetiPassword"]
+  if Password == "":
+    return render_template("errore2.html")
+  elif RipetiPassword == "":
+    return render_template("errore2.html")
+  elif Nome == "":
+    return render_template("errore2.html")
+  elif Cognome == "":
+    return render_template("errore2.html")
+  elif Email == "":
+    return render_template("errore2.html")
+  elif Password!=RipetiPassword:
+    return render_template("errore1.html")
+  else:
+    return redirect(url_for('base_page'))
+
+@app.route('/shop')
+def base_page():
+  return render_template("base.html")
+
+@app.route('/contact-us')
+def contact_us():
+	return render_template("contact-us.html")
+
+@app.route('/about-us')
+def about_us():
+	return render_template("about-us.html")
+
+@app.route('/aleks')
+def aleks():
+	return render_template('aleks.html')
+
+if __name__ == "__main__":
+  app.run(
+		host='0.0.0.0',
+		port=8080,
+    debug=True
+	)
